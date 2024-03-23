20cd73384e383d270acced66a69e7858c1f0f8c4
lerema <leonardoreis.m@hotmail.com>
2024-03-23 09:41:05 -0300

    testing: implementing asserts on requests

--- src/infra/swapi_api_consumer_test.py
+++ src/infra/swapi_api_consumer_test.py
@@ -1,12 +1,17 @@
 from .swap_api_consumer import SwapiApiConsumer
 
-def test_get_starships(requests_mock):
+def test_get_starships():
     ''' Testing get_startships method '''
     
-    requests_mock.get('https://swapi.dev/api/starships/', status_code=200, json={ 'some':  'thing'})
+    # requests_mock.get('https://swapi.dev/api/starships/', status_code=200, json={ 'some':  'thing'})
     
     swapi_api_consumer = SwapiApiConsumer()
-    response = swapi_api_consumer.get_starships(page=1)
+    page = 1
+    get_star_ship_response = swapi_api_consumer.get_starships(page=page)
     
-    print(response)
+    assert get_star_ship_response.request.method == 'GET'
+    assert get_star_ship_response.request.url == 'https://swapi.dev/api/starships/'
+    assert get_star_ship_response.request.params == {"page": page }
     
+    assert get_star_ship_response.status_code == 200
+    assert isinstance(get_star_ship_response.response["results"], list)
